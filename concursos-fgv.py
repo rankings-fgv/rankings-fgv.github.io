35a7ee23991d9163d99bc7d65a21b518449591f3
Allan M. de Azevedo <4lln.medeiros@gmail.com>
2023-01-20 08:44:54 -0300

    Add json file with modified time for each item and Telegram bot integration
    Update GitHub workflow to run every 10 minutes and use the secret to Telegram Bot.
    
    t.me/rankingsFGV

--- concursos-fgv.py
+++ concursos-fgv.py
@@ -1,36 +1,48 @@
+import argparse
+import os
+import json
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-concursos = {
-    "tjdft22": {},
-    "tjto22": {},
-    "tceto22": {},
-    "senado22/1": {},
-    "senado22/2": {},
-    "senado22/3": {},
-    "senado22/4": {},
-    "senado22/5": {},
-    "trt16": {},
-    "trt13": {},
-    "sefmg22": {},
-}
+parser = argparse.ArgumentParser()
+parser.add_argument("--token", type=str, help="Telegram bot authentication token")
+args = parser.parse_args()
 
-for concurso in concursos:
-    url = f"https://conhecimento.fgv.br/concursos/{concurso}"
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
-    h1_element = soup.find("h1", id="page-title")
-    table_element = soup.find("table")
-    tr_element = table_element.find_all("tr")[1]
-    td_elements = tr_element.find_all("td")
-    td_p_elements = td_elements[1].find_all("p")
-    concursos[concurso]["nome"] = h1_element.text.strip()[24:]
-    concursos[concurso]["data"] = datetime.strptime(td_elements[0].text, "%d/%m/%Y")
-    concursos[concurso]["texto"] = td_p_elements[0].text if len(td_p_elements) > 0 else td_elements[1].text
+basename = os.path.splitext(os.path.basename(__file__))[0]
+
+with open(f"{basename}.json", "r") as json_file_r:
+    json_concursos = json.load(json_file_r)
+    tmp_concursos = {}
+
+    for concurso in json_concursos:
+        tmp_concursos[concurso] = {}
+        url = f"https://conhecimento.fgv.br/concursos/{concurso}"
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, "html.parser")
+        meta__modified_time = soup.find("meta", property="article:modified_time")
+        h1_element = soup.find("h1", id="page-title")
+        table_element = soup.find("table")
+        tr_element = table_element.find_all("tr")[1]
+        td_elements = tr_element.find_all("td")
+        td_p_elements = td_elements[1].find_all("p")
+        tmp_concursos[concurso]["nome"] = h1_element.text.strip()[24:]
+        tmp_concursos[concurso]["data"] = datetime.strptime(td_elements[0].text, "%d/%m/%Y")
+        tmp_concursos[concurso]["data_modificacao"] = meta__modified_time.attrs["content"]
+        tmp_concursos[concurso]["texto"] = td_p_elements[0].text if len(td_p_elements) > 0 else td_elements[1].text
+        #
+        if args.token:
+            if json_concursos[concurso] != tmp_concursos[concurso]["data_modificacao"]:
+                message = requests.utils.quote("A página do Concurso Público para o {} ({}) foi atualizada\n\nÚltima atualização: {}\n\n{}: {}\n\nhttps://conhecimento.fgv.br/concursos/{}".format(tmp_concursos[concurso]["nome"], concurso, datetime.fromisoformat(tmp_concursos[concurso]["data_modificacao"]).strftime("%d/%m/%Y %H:%M:%S"), tmp_concursos[concurso]["data"].strftime("%d/%m/%Y"), tmp_concursos[concurso]["texto"], concurso.replace("/", "-")))
+                url_bot = f"https://api.telegram.org/bot{args.token}/sendMessage?chat_id=@rankingsFGV&text={message}"
+                requests.get(url_bot)
+        json_concursos[concurso] = tmp_concursos[concurso]["data_modificacao"]
+    
+with open(f"{basename}.json", "w") as json_file_w:
+    json.dump(json_concursos, json_file_w, indent=4, default=str)
 
 print("# Concursos")
 
-for c in sorted(concursos.items(), key=lambda x: x[1]["data"], reverse=True):
+for c in sorted(tmp_concursos.items(), key=lambda x: x[1]["data"], reverse=True):
     print("\n## [{}](./{}/) *({})*".format(c[1]["nome"], c[0].replace("/", "-"), c[0]))
     print("{}: {}".format(c[1]["data"].strftime("%d/%m/%Y"), c[1]["texto"]))
