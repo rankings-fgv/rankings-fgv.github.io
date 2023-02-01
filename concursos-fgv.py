f6890781685ced56f14914aeaf9cd1079ff59628
Allan M. de Azevedo <4lln.medeiros@gmail.com>
2023-02-01 09:09:14 -0300

    Replace meta "article:modified_time" with CRC32 checksum
    The FGV website removed the meta tag containing the "modified_time".
    
    The checksum will compute a CRC of table_element text.

--- concursos-fgv.py
+++ concursos-fgv.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import json
 import requests
+import zlib
 from bs4 import BeautifulSoup
 from datetime import datetime
 
@@ -20,23 +21,22 @@ for concurso in json_concursos:
     url = f"https://conhecimento.fgv.br/concursos/{concurso}"
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
-    meta__modified_time = soup.find("meta", property="article:modified_time")
-    h1_element = soup.find("h1", id="page-title")
+    h1_element = soup.find("h1")
     table_element = soup.find("table")
     tr_element = table_element.find_all("tr")[1]
     td_elements = tr_element.find_all("td")
     td_p_elements = td_elements[1].find_all("p")
     tmp_concursos[concurso]["nome"] = h1_element.text.strip()[24:]
     tmp_concursos[concurso]["data"] = datetime.strptime(td_elements[0].text, "%d/%m/%Y")
-    tmp_concursos[concurso]["data_modificacao"] = meta__modified_time.attrs["content"]
+    tmp_concursos[concurso]["crc32"] = hex(zlib.crc32(table_element.text.encode()) & 0xffffffff)
     tmp_concursos[concurso]["texto"] = td_p_elements[0].text if len(td_p_elements) > 0 else td_elements[1].text
     #
     if args.token:
-        if json_concursos[concurso] != tmp_concursos[concurso]["data_modificacao"]:
-            message = requests.utils.quote("A página do Concurso Público para o(a) {} ({}) foi atualizada\n\nÚltima atualização: {}\n\n{}: {}\n\nhttps://conhecimento.fgv.br/concursos/{}".format(tmp_concursos[concurso]["nome"], concurso, datetime.fromisoformat(tmp_concursos[concurso]["data_modificacao"]).strftime("%d/%m/%Y %H:%M:%S"), tmp_concursos[concurso]["data"].strftime("%d/%m/%Y"), tmp_concursos[concurso]["texto"], concurso))
+        if json_concursos[concurso] != tmp_concursos[concurso]["crc32"]:
+            message = requests.utils.quote("A página do Concurso Público para o(a) {} ({}) foi atualizada\n\n{}: {}\n\nhttps://conhecimento.fgv.br/concursos/{}".format(tmp_concursos[concurso]["nome"], concurso, tmp_concursos[concurso]["data"].strftime("%d/%m/%Y"), tmp_concursos[concurso]["texto"], concurso))
             url_bot = f"https://api.telegram.org/bot{args.token}/sendMessage?chat_id=@rankingsFGV&text={message}"
             requests.get(url_bot)
-    json_concursos[concurso] = tmp_concursos[concurso]["data_modificacao"]
+    json_concursos[concurso] = tmp_concursos[concurso]["crc32"]
     
 with open(f"{basename}.json", "w") as json_file_w:
     json.dump(json_concursos, json_file_w, indent=4, default=str)
